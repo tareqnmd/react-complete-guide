2ce171f717730c75e6001cde5c479ca00e17a710
Tareq <tareqnmd@gmail.com>
2022-02-17 09:09:12 +0600

    restructure

---
+++ src/components/Card.js
@@ -0,0 +1,7 @@
+import './Card.css';
+const Card = ({ children, className }) => {
+	const classes = `card ${className}`;
+	return <div className={classes}>{children}</div>;
+};
+
+export default Card;
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import Card from './Card';
 import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
 
 const Expenses = ({ expenses }) => {
 	return (
-		<>
+		<Card className="expenses">
 			{expenses.map((expense) => (
 				<ExpenseItem key={expense.id} expense={expense} />
 			))}
-		</>
+		</Card>
 	);
 };
 
