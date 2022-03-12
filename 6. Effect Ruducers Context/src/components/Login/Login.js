a8522215b21e88f05aaaa552a90cc3f932a27402
tareqnmd <tareqnmd@gmail.com>
2022-03-12 11:11:35 +0600

    useRef And f\orwardRef

--- 6. Effect Ruducers Context/src/components/Login/Login.js
+++ 6. Effect Ruducers Context/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useReducer, useState } from 'react';
+import React, { useContext, useEffect, useReducer, useState, useRef } from 'react';
 import AuthContext from '../../store/auth-context';
 import Button from '../UI/Button/Button';
 import Card from '../UI/Card/Card';
@@ -46,6 +46,8 @@ const Login = () => {
 	// const [emailIsValid, setEmailIsValid] = useState();
 	// const [enteredPassword, setEnteredPassword] = useState('');
 	// const [passwordIsValid, setPasswordIsValid] = useState();
+	const emailInputRef = useRef();
+	const passwordInputRef = useRef();
 	const authCtx = useContext(AuthContext);
 	const [formIsValid, setFormIsValid] = useState(false);
 
@@ -83,7 +85,13 @@ const Login = () => {
 
 	const submitHandler = (event) => {
 		event.preventDefault();
-		authCtx.onLogin(emailState.value, passwordState.value);
+		if (formIsValid) {
+			authCtx.onLogin(emailState.value, passwordState.value);
+		} else if (!emailIsValid) {
+			emailInputRef.current.focus();
+		} else {
+			passwordInputRef.current.focus();
+		}
 	};
 
 	// useEffect(() => {
@@ -119,6 +127,7 @@ const Login = () => {
 		<Card className={classes.login}>
 			<form onSubmit={submitHandler}>
 				<Input
+					ref={emailInputRef}
 					id="email"
 					label="Email"
 					type="email"
@@ -129,6 +138,7 @@ const Login = () => {
 					onChange={emailChangeHandler}
 				/>
 				<Input
+					ref={passwordInputRef}
 					type="password"
 					id="password"
 					value={passwordState.value}
@@ -139,7 +149,8 @@ const Login = () => {
 					onChange={passwordChangeHandler}
 				/>
 				<div className={classes.actions}>
-					<Button type="submit" className={classes.btn} disabled={!formIsValid}>
+					{/* <Button type="submit" className={classes.btn} disabled={!formIsValid}> */}
+					<Button type="submit" className={classes.btn}>
 						Login
 					</Button>
 				</div>
--- 6. Effect Ruducers Context/src/components/UI/Input/Input.js
+++ 6. Effect Ruducers Context/src/components/UI/Input/Input.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { forwardRef, useImperativeHandle, useRef } from 'react';
 import classes from './Input.module.css';
 
-const Input = ({ isValid, id, label, type, value, onChange, onBlur }) => {
+const Input = forwardRef(({ isValid, id, label, type, value, onChange, onBlur }, ref) => {
+	const inputRef = useRef();
+
+	const activeFocus = () => {
+		inputRef.current.focus();
+	};
+
+	useImperativeHandle(ref, () => {
+		return { focus: activeFocus };
+	});
+
 	return (
 		<div className={`${classes.control} ${isValid === false ? classes.invalid : ''}`}>
 			<label htmlFor={id}>{label}</label>
-			<input type={type} id={id} value={value} onChange={onChange} onBlur={onBlur} />
+			<input ref={inputRef} type={type} id={id} value={value} onChange={onChange} onBlur={onBlur} />
 		</div>
 	);
-};
+});
 
 export default Input;
