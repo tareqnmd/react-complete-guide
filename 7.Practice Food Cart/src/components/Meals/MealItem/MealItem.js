dc4d23cedc7cc4f2fdc126636b91372c4cb2d112
tareqnmd <tareqnmd@gmail.com>
2022-03-12 15:27:34 +0600

    updated

--- 7.Practice Food Cart/src/components/Meals/MealItem/MealItem.js
+++ 7.Practice Food Cart/src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 const MealItem = ({ meal }) => {
 	const { name, description, price } = meal;
@@ -9,7 +10,9 @@ const MealItem = ({ meal }) => {
 				<div className={classes.description}>{description}</div>
 				<div className={classes.price}>{price}</div>
 			</div>
-			<div></div>
+			<div>
+				<MealItemForm />
+			</div>
 		</li>
 	);
 };
