9de432a8176bd9d525554ad44999d928edb8bbb6
tareqnmd <tareqnmd@gmail.com>
2022-03-13 10:06:07 +0600

    cart modal functionality added

--- 7.Practice Food Cart/src/store/CartProvider.js
+++ 7.Practice Food Cart/src/store/CartProvider.js
@@ -1,12 +1,38 @@
+import { useReducer } from 'react';
 import CartContext from './cart-context';
 
-const CartProvider = ({children}) => {
-	const addItemHandler = (item) => {};
-	const removeItemHandler = (id) => {};
+const defaultCartState = {
+	items: [],
+	totalAmount: 0,
+};
+
+const carReducer = (state, action) => {
+	if (action.type === 'ADD') {
+		const updatedItems = state.items.concat(action.item);
+		const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
+	} else if (action.type === 'REMOVE') {
+	}
+	return defaultCartState;
+};
+
+const CartProvider = ({ children }) => {
+	const [cartState, dispatchCartAction] = useReducer(carReducer, defaultCartState);
+
+	const addItemHandler = (item) => {
+		dispatchCartAction({ type: 'ADD', item: item });
+	};
+
+	const removeItemHandler = (id) => {
+		dispatchCartAction({ type: 'REMOVE', id: id });
+	};
 
 	const cartContext = {
-		items: [],
-		totalAmount: 0,
+		items: cartState.items,
+		totalAmount: cartState.totalAmount,
 		addItem: addItemHandler,
 		removeItem: removeItemHandler,
 	};
