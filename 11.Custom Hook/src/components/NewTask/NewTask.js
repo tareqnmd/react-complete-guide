186f328cf775391efe6f1d3f22993fd03f678ce8
Tareq <tareqnmd@gmail.com>
2022-04-01 11:38:56 +0600

    http hook

--- 11.Custom Hook/src/components/NewTask/NewTask.js
+++ 11.Custom Hook/src/components/NewTask/NewTask.js
@@ -1,40 +1,27 @@
-import { useState } from 'react';
+import useHttp from '../../hooks/use-http';
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
 
 const NewTask = (props) => {
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState(null);
+	const { isLoading, error, SendRequest: sendTaskRequest } = useHttp();
+
+	const createTask = (taskText, data) => {
+		const generatedId = data.name;
+		const createdTask = { id: generatedId, text: taskText };
+
+		props.onAddTask(createdTask);
+	};
 
 	const enterTaskHandler = async (taskText) => {
-		setIsLoading(true);
-		setError(null);
-		try {
-			const response = await fetch(
-				'https://react-http-tareq-default-rtdb.firebaseio.com/tasks.json',
-				{
-					method: 'POST',
-					body: JSON.stringify({ text: taskText }),
-					headers: {
-						'Content-Type': 'application/json',
-					},
-				}
-			);
-
-			if (!response.ok) {
-				throw new Error('Request failed!');
-			}
-
-			const data = await response.json();
-
-			const generatedId = data.name; // firebase-specific => "name" contains generated id
-			const createdTask = { id: generatedId, text: taskText };
-
-			props.onAddTask(createdTask);
-		} catch (err) {
-			setError(err.message || 'Something went wrong!');
-		}
-		setIsLoading(false);
+		sendTaskRequest(
+			{
+				url: 'https://react-http-tareq-default-rtdb.firebaseio.com/tasks.json',
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: { text: taskText },
+			},
+			createTask.bind(null, taskText)
+		);
 	};
 
 	return (
