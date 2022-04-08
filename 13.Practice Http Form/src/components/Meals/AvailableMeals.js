62e70f5caa8a61e1b288cf6f8addfdf9b385012e
Tareq <tareqnmd@gmail.com>
2022-04-08 22:18:52 +0600

    update

--- 13.Practice Http Form/src/components/Meals/AvailableMeals.js
+++ 13.Practice Http Form/src/components/Meals/AvailableMeals.js
@@ -6,27 +6,39 @@ import MealItem from './MealItem/MealItem';
 const AvailableMeals = () => {
 	const [meals, setMeals] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState();
 	useEffect(() => {
 		const fetchMeals = async () => {
-			const response = await fetch(
-				'https://react-http-tareq-default-rtdb.firebaseio.com/meals.json'
-			);
-			const data = await response.json();
-			const processedData = [];
-			for (const key in data) {
-				processedData.push({
-					...data[key],
-					id: key,
-				});
+			try {
+				const response = await fetch(
+					'https://react-http-tareq-default-rtdb.firebaseio.com/meals.json'
+				);
+				if (!response.ok) {
+					throw new Error('Something went wrong!');
+				}
+				const data = await response.json();
+				const processedData = [];
+				for (const key in data) {
+					processedData.push({
+						...data[key],
+						id: key,
+					});
+				}
+				setMeals(processedData);
+			} catch (error) {
+				setError(error.message);
 			}
-			setMeals(processedData);
 			setIsLoading(false);
 		};
+
 		fetchMeals();
 	}, []);
 
 	if (isLoading) {
 		return <div className={classes.loading}>Loading...</div>;
+	}
+	if (error) {
+		return <div className={classes.error}>{error}</div>;
 	}
 
 	const mealsList = meals.map((meal) => (
