e97925531dfd011e8fa49e10025a15fe3aa29170
Tareq <tareqnmd@gmail.com>
2022-04-12 22:35:12 +0600

    updated redux

--- 14.Dive Into Redux/src/index.js
+++ 14.Dive Into Redux/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
+import store from './store/index';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
 	<React.StrictMode>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</React.StrictMode>
 );
