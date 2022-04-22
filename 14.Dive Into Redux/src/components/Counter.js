50d4ecf3fca6ccbddb325d76b1f95029c096b3d8
Tareq <tareqnmd@gmail.com>
2022-04-22 11:36:42 +0600

    up redux store

--- 14.Dive Into Redux/src/components/Counter.js
+++ 14.Dive Into Redux/src/components/Counter.js
@@ -1,15 +1,30 @@
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+	const counter = useSelector((state) => state.counter);
+	const dispatch = useDispatch();
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
+	const incrementHandler = () => {
+		dispatch({ type: 'Increment' });
+	};
+	const decrementHandler = () => {
+		dispatch({ type: 'Decrement' });
+	};
+
+	const toggleCounterHandler = () => {};
+
+	return (
+		<main className={classes.counter}>
+			<h1>Redux Counter</h1>
+			<div className={classes.value}>{counter}</div>
+			<div>
+				<button onClick={incrementHandler}>Increment</button>
+				<button onClick={decrementHandler}>Decrement</button>
+			</div>
+			<button onClick={toggleCounterHandler}>Toggle Counter</button>
+		</main>
+	);
 };
 
 export default Counter;
