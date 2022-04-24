9ec28a6c1dfafa831c9d34df57c2e0bfc849a788
Tareq <tareqnmd@gmail.com>
2022-04-24 11:16:03 +0600

    up

--- 15.Redux Deeper/src/App.js
+++ 15.Redux Deeper/src/App.js
@@ -4,7 +4,8 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { uiActions } from './store/reducers/ui-slice';
+import { sendCartData } from './store/reducers/cart-slice';
+// import { uiActions } from './store/reducers/ui-slice';
 let isInitial = true;
 
 function App() {
@@ -14,47 +15,48 @@ function App() {
 	const notification = useSelector((state) => state.ui.notification);
 
 	useEffect(() => {
-		const sendCartData = async () => {
-			dispatch(
-				uiActions.showNotification({
-					status: 'pending',
-					title: 'Sending...',
-					message: 'Sending cart data',
-				})
-			);
-			try {
-				const response = await fetch(
-					'https://redux-cart-6dcd9-default-rtdb.firebaseio.com/cart.json',
-					{
-						method: 'PUT',
-						body: JSON.stringify(cart),
-					}
-				);
-				if (!response.ok) {
-					throw new Error('Sending cart data failed!');
-				}
-				dispatch(
-					uiActions.showNotification({
-						status: 'success',
-						title: 'Success!',
-						message: 'Sent cart data Successfully',
-					})
-				);
-			} catch (error) {
-				dispatch(
-					uiActions.showNotification({
-						status: 'error',
-						title: 'Error!',
-						message: 'Sent cart data Failed',
-					})
-				);
-			}
-		};
+		// const sendCartData = async () => {
+		// 	dispatch(
+		// 		uiActions.showNotification({
+		// 			status: 'pending',
+		// 			title: 'Sending...',
+		// 			message: 'Sending cart data',
+		// 		})
+		// 	);
+		// 	try {
+		// 		const response = await fetch(
+		// 			'https://redux-cart-6dcd9-default-rtdb.firebaseio.com/cart.json',
+		// 			{
+		// 				method: 'PUT',
+		// 				body: JSON.stringify(cart),
+		// 			}
+		// 		);
+		// 		if (!response.ok) {
+		// 			throw new Error('Sending cart data failed!');
+		// 		}
+		// 		dispatch(
+		// 			uiActions.showNotification({
+		// 				status: 'success',
+		// 				title: 'Success!',
+		// 				message: 'Sent cart data Successfully',
+		// 			})
+		// 		);
+		// 	} catch (error) {
+		// 		dispatch(
+		// 			uiActions.showNotification({
+		// 				status: 'error',
+		// 				title: 'Error!',
+		// 				message: 'Sent cart data Failed',
+		// 			})
+		// 		);
+		// 	}
+		// };
 		if (isInitial) {
 			isInitial = false;
 			return;
 		}
-		sendCartData();
+		// sendCartData();
+		dispatch(sendCartData(cart));
 	}, [cart, dispatch]);
 
 	return (
--- 15.Redux Deeper/src/store/reducers/cart-slice.js
+++ 15.Redux Deeper/src/store/reducers/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { uiActions } from './ui-slice';
 
 const initialState = {
 	items: [],
@@ -38,6 +39,46 @@ const cartSlice = createSlice({
 		},
 	},
 });
+
+export const sendCartData = (cart) => {
+	return async (dispatch) => {
+		dispatch(
+			uiActions.showNotification({
+				status: 'pending',
+				title: 'Sending...',
+				message: 'Sending cart data',
+			})
+		);
+
+		try {
+			const response = await fetch(
+				'https://redux-cart-6dcd9-default-rtdb.firebaseio.com/cart.json',
+				{
+					method: 'PUT',
+					body: JSON.stringify(cart),
+				}
+			);
+			if (!response.ok) {
+				throw new Error('Sending cart data failed!');
+			}
+			dispatch(
+				uiActions.showNotification({
+					status: 'success',
+					title: 'Success!',
+					message: 'Sent cart data Successfully',
+				})
+			);
+		} catch (error) {
+			dispatch(
+				uiActions.showNotification({
+					status: 'error',
+					title: 'Error!',
+					message: 'Sent cart data Failed',
+				})
+			);
+		}
+	};
+};
 
 export const cartActions = cartSlice.actions;
 
