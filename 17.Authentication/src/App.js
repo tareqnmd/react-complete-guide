dc7f25f4302c8a2f0ec4d8444285cad3e02a60e3
tareqnmd <tareqnmd@gmail.com>
2022-05-21 16:55:51 +0600

    up

--- 17.Authentication/src/App.js
+++ 17.Authentication/src/App.js
@@ -1,26 +1,33 @@
-import { Switch, Route } from 'react-router-dom';
-
+import { useContext } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import AuthContext from './store/auth-context';
 
 function App() {
-  return (
-    <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <HomePage />
-        </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
-        </Route>
-      </Switch>
-    </Layout>
-  );
+	const authCtx = useContext(AuthContext);
+	return (
+		<Layout>
+			<Switch>
+				<Route path="/" exact>
+					<HomePage />
+				</Route>
+				{!authCtx.isLoggedIn && (
+					<Route path="/auth">
+						<AuthPage />
+					</Route>
+				)}
+				<Route path="/profile">
+					{authCtx.isLoggedIn ? <UserProfile /> : <Redirect to="/auth" />}
+				</Route>
+				<Route path="*">
+					<Redirect to="/" />
+				</Route>
+			</Switch>
+		</Layout>
+	);
 }
 
 export default App;
